48dd43a4771c1c26edcd588cf7c009c3a6bc76eb
Trong Tran <gacontuyenchien1@gmail.com>
2022-09-22 01:51:11 +0700

    ReactPhoneBook project, finish v1.0.0

--- Study Cases/JavaScript/ReactPhoneBook/Readme.md
+++ Study Cases/JavaScript/ReactPhoneBook/Readme.md
@@ -1,4 +1,4 @@
 # Reference:
 
 1. [Source reference](https://codesandbox.io/s/8nyki?file=/src/index.js)
-2. []()
+2. [The problem detail]()
---
+++ Study Cases/JavaScript/ReactPhoneBook/src/components/PhoneBookList.js
@@ -0,0 +1,39 @@
+const style = {
+    container : {
+        margin: "10px 10px 10px 10px",
+        padding: "0 10px 0 10px",
+        border: "1px solid #F0F8FF"
+    },
+    h1: {
+        marginLeft: "-10px",
+    }
+}
+
+function PhoneBookList( props ){
+    const printUserList = ( users ) => {
+        return users.map( user => 
+            <tr>
+                <td>{user.fname}</td>
+                <td>{user.lname}</td>
+                <td>{user.phone}</td>
+            </tr>);
+    }
+
+    return <div style={style.container}>
+        <h1 style={style.h1}>Phone Book List</h1>
+        <table className="table table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">First name</th>
+                    <th scope="col">Last name</th>
+                    <th scope="col">Number</th>
+                </tr>
+            </thead>
+            <tbody>
+                {printUserList( props.users )}
+            </tbody>
+        </table>
+    </div>;
+}
+
+export default PhoneBookList;
